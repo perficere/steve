d500eb9989a7486d81a3e972d406e92c37b1a1f8
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-04-24 19:10:47 -0400

    feat: add health check methods to exchange interfaces

--- steve/arbitrage/exchanges/binance.py
+++ steve/arbitrage/exchanges/binance.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 from binance.client import Client
+from binance.exceptions import BinanceAPIException as APIException
 
 from utils.metaclasses import Singleton
 
@@ -8,3 +9,12 @@ from utils.metaclasses import Singleton
 class Interface(metaclass=Singleton):
     def __init__(self):
         self.client = Client(settings.BINANCE_API_KEY, settings.BINANCE_API_SECRET)
+
+    @property
+    def healthy(self):
+        try:
+            response = self.client.get_system_status()
+        except APIException:
+            return False
+
+        return response["status"] == 0
--- steve/arbitrage/exchanges/buda.py
+++ steve/arbitrage/exchanges/buda.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 from trading_api_wrappers import Buda as Client
+from trading_api_wrappers.errors import APIException
 
 from utils.metaclasses import Singleton
 
@@ -8,3 +9,12 @@ from utils.metaclasses import Singleton
 class Interface(metaclass=Singleton):
     def __init__(self):
         self.client = Client.Auth(settings.BUDA_API_KEY, settings.BUDA_API_SECRET)
+
+    @property
+    def healthy(self):
+        try:
+            response = self.client.markets()
+        except APIException:
+            return False
+
+        return bool(response)
